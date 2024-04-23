4156d5f092eda63b5c57dc524a5b007e50803be1
anil2003shukla <shuklaanil0807@gmail.com>
2024-04-24 00:32:23 +0530

    third commit

--- Arrays.js
+++ Arrays.js
@@ -8,12 +8,35 @@ let myArr=[1,2,6,3,4,3,4]
 // console.log(typeof newArr);
 // console.log(myArr); 
 // slice splice
-console.log("A",myArr);
+// console.log("A",myArr);
  
-const myn1=myArr.slice(1,3)
-console.log(myn1);
-console.log("B",myArr);
-const myn2=myArr.splice(1,3);
-console.log(myn2);
-console.log("C",myArr);
+// const myn1=myArr.slice(1,3)
+// console.log(myn1);
+// console.log("B",myArr);
+// const myn2=myArr.splice(1,3);
+// console.log(myn2);
+// console.log("C",myArr);
 
+
+const marvel_heros=["thor","IronMan","spiderMan"];
+const dc_heros=["superman","batman","wonder Woman"];
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+//  const Allheros=marvel_heros.concat(dc_heros);
+// console.log(Allheros);
+//spread operator
+// const all_new_heros =[...marvel_heros,...dc_heros];
+// // console.log(all_new_heros);
+// const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]];
+// const real_another_array= another_array.flat(Infinity);
+
+// console.log(real_another_array);
+// console.log(Array.isArray("Anil"));
+// console.log(Array.from("Anil"));
+
+// //Intresting In Interviews
+// console.log(Array.from({name:"Anil"}));
+// let score1=100;
+// let score2=200;
+// let score3=300;
+// console.log(Array.of(score1,score2,score3));
--- Dates.js
+++ Dates.js
@@ -1,6 +1,6 @@
 //dates
-let myDate=new Date();
-// console.log(myDate.toString());//Fri Mar 01 
+// let myDate=new Date();
+// // console.log(myDate.toString());//Fri Mar 01 
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleDateString());
 // console.log( typeof myDate);
@@ -9,15 +9,15 @@ let myDate=new Date();
 // let myCreatedDate=new Date("2023-03-13");
 // console.log(myCreatedDate.toDateString())
 // console.log(myCreatedDate.toLocaleString());
-let myTimestamp=Date.now();
-// console.log(Math.floor(Date.now()/1000))
-// console.log(myTimestamp);
-let newDate=new Date();
-console.log(newDate);
-console.log(newDate.getMonth()+1);
-console.log(newDate.getDay());
-console.log(newDate.toLocaleString('default',{
-    weekday:'long',
-    month: 'long',
-    day:'2-digit'
-}));
+// let myTimestamp=Date.now();
+// // console.log(Math.floor(Date.now()/1000))
+// // console.log(myTimestamp);
+// let newDate=new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+// console.log(newDate.toLocaleString('default',{
+//     weekday:'long',
+//     month: 'long',
+//     day:'2-digit'
+// }));
