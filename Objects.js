fbb3ab45e6c2a72cf91713da6f83597639b23ece
anil2003shukla <shuklaanil0807@gmail.com>
2024-04-24 00:39:32 +0530

    Many Commit

---
+++ Objects.js
@@ -0,0 +1,11 @@
+//objects literals
+const JsUser={
+    name:"Anil",
+    age:21,
+    location:"Lucknow",
+    email:"anil.kumar@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"]
+}
+console.log(JsUser.email);
+console.log(JsUser["email"]);
